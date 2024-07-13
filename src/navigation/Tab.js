67843d4a0cf5db928ffbf5dc8443aa577ebd1b22
Sam <samuel@encodevlabs.com>
2024-07-13 22:11:17 +0000

    added initial routes

--- src/navigation/Tab.js
+++ src/navigation/Tab.js
@@ -5,7 +5,10 @@ const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
  return (
-  <Tab.Navigator screenOptions={{ headerShown: false }}>
+  <Tab.Navigator
+  initialRouteName="First"
+  screenOptions={{ headerShown: false }}
+  >
    <Tab.Screen name="First">{() => <HomeStack />}</Tab.Screen>
    <Tab.Screen name="Second">{() => <SettingsStack />}</Tab.Screen>
   </Tab.Navigator>
